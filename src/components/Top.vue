bddcdfd4c68b764c3f6d8a9648005fc53f491d28
Priya X. Pramesi <ppramesi@visi8.com>
2021-12-20 12:47:50 +0700

    init

---
+++ src/components/Top.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="top-background" :style="'background-image:linear-gradient(to top, ' + color + ', rgb(0 0 0 / 1%)), url(' + background + ');'">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Top',
+    props: {
+        background: {
+            type: String,
+            default: ''
+        },
+        color: {
+            type: String,
+            default: 'black'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.top-background{
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,37 +1,51 @@
 <template>
-    <div class="container h-screen flex flex-col justify-center px-6">
+    <div class="container h-screen">
         <!-- center the content below -->
-        <div class="flex flex-col items-center">
-            <div>Welcome To</div>
-            <div>Experience of Indonesia</div>
-            <hr>
-            <div>a bunch of text</div>
-            <div>
-                <div class="flex flex-row">
-                    <div>
-                        <router-link to="/sight">
-                            [button here]
-                        </router-link>
-                    </div>
-                    <div>
-                        <router-link to="/taste">
-                            [button here]
-                        </router-link>
-                    </div>
-                    <div>
-                        <router-link to="/sound">
-                            [button here]
-                        </router-link>
+        <div class="flex flex-col">
+            <top class="h-screen w-screen items-center flex flex-col justify-center" :background="welcomeBackground">
+                <div>Welcome To</div>
+                <div>Experience of Indonesia</div>
+                <hr>
+                <div>a bunch of text</div>
+                <div>
+                    <div class="flex flex-row">
+                        <div>
+                            <router-link to="/sight">
+                                [button here]
+                            </router-link>
+                        </div>
+                        <div>
+                            <router-link to="/taste">
+                                [button here]
+                            </router-link>
+                        </div>
+                        <div>
+                            <router-link to="/sound">
+                                [button here]
+                            </router-link>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </top>
         </div>
     </div>
 </template>
 
 <script>
+import Top from '../components/Top.vue'
 export default {
-    
+    name: 'welcome',
+    components: {
+        Top
+    },
+    data(){
+        return {
+            welcomeBackground: require('../assets/Borobudur.jpg')
+        }
+    },
+    created(){
+        console.log(this.welcomeBackground)
+    }
 }
 </script>
 
