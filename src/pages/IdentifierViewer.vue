8095644701e71f5cde7a4352707e5f42a3c96eb1
Priya X. Pramesi <ppramesi@visi8.com>
2022-01-18 14:41:58 +0700

    changeable connection details

--- src/pages/IdentifierViewer.vue
+++ src/pages/IdentifierViewer.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-        {{ write }}
+    <div class="p-4">
+        <div>
+            <div>
+                <label for="">UUID</label>
+                <br>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="write.identifier">
+            </div>
+            <br>
+            <div>
+                <label for="">Connection ID</label>
+                <br>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="write.id">
+            </div>
+        </div>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="setConnectionIdentifier">Set connection identifier</button>
     </div>
 </template>
 
@@ -8,13 +21,22 @@
 export default {
     data(){
         return {
-            write: ''
+            write: {
+                identifier: '',
+                id: ''
+            }
+        }
+    },
+    methods: {
+        setConnectionIdentifier(){
+            window.localStorage.setItem('uuid', this.write.identifier)
+            window.localStorage.setItem('id', this.write.id)
         }
     },
     created(){
         const connectionUuid = window.localStorage.getItem('uuid')
         const connectionId = window.localStorage.getItem('id')
-        this.write = JSON.stringify({identifier: connectionUuid, id: connectionId})
+        this.write = {identifier: connectionUuid, id: connectionId}
     }
 }
 </script>
