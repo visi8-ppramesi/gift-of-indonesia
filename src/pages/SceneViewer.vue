c7ad59a3d96e8cdb3d93cb4ae3cf2fac5f5c5086
Priya X. Pramesi <ppramesi@visi8.com>
2022-01-18 17:59:59 +0700

    fixed

--- src/pages/SceneViewer.vue
+++ src/pages/SceneViewer.vue
@@ -13,7 +13,7 @@ export default {
 <a-scene>
     <a-videosphere rotation="0 180 0" src="#video">
     </a-videosphere>
-    <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+    <!-- <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane> -->
     <!-- The original example also has this 180 degree rotation, to appear to be going forward. --><!-- <a-videosphere src="#video"></a-videosphere> -->
     <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
     <!-- Define camera with zero user height, movement disabled and arrow key rotation added. --><!-- <a-camera user-height="0"><a-entity id="msg" position="0 -0.3 -1.5" text="align:center; width:3; wrapCount:100; color:red;"></a-entity></a-camera>--><!-- Wait for the video to load. -->
