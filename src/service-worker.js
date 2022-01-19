07a78b3bf45868c0446ba9c8f9727baa21ba13a4
Priya X. Pramesi <ppramesi@visi8.com>
2022-01-19 15:39:39 +0700

    changed service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -15,11 +15,50 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 workbox.core.setCacheNameDetails({prefix: "gift-of-indonesia"});
 
+var CACHE_NAME = 'gift-of-indonesia-v1';
+var urlsToPrefetch = [
+  'https://firebasestorage.googleapis.com/v0/b/gift-of-indonesia.appspot.com/o/city.mp4?alt=media&token=1c9c4547-37d2-41cc-bdf0-a0fcb81bcff2'
+];
+
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToPrefetch);
+    })
+  );
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log(event)
+  if (event.request.url.startsWith('chrome-extension://')) return;
+  if (event.request.method != 'GET') return;
+
+
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+      // If we found a match in the cache, return it, but also
+      // update the entry in the cache in the background.
+      event.waitUntil(cache.add(event.request));
+      return cachedResponse;
+    }
+
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
+})
 
 console.log('testing')
 
