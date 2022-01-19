bbf99b8123114a54f4adae9236443562f1d32eda
Priya X. Pramesi <ppramesi@visi8.com>
2022-01-19 14:11:59 +0700

    added user agent

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Version {{ version }}
+    Version {{ version }}, User Agent {{ userAgent }}
     <nav @click="goBack" v-if="$route.name !== 'welcome'" class="go-back-container fixed h-16">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-16" fill="white" viewBox="0 0 24 24" stroke="black">
             <path stroke-linecap="round" stroke-linejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
@@ -23,11 +23,13 @@ export default {
     data(){
         return {
             testing: require('./scenes/testing.html'),
-            version: '0.0.0'
+            version: '0.0.0',
+            userAgent: '',
         }
     },
     created(){
         this.version = process.env.VUE_APP_VERSION
+        this.userAgent = navigator.userAgent
         console.log(this.version)
         const self = this
         if(this.$isOculus || this.$isMobile){
