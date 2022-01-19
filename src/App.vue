5053462848135a0d20ba2adf566c323baabcf088
Priya X. Pramesi <ppramesi@visi8.com>
2022-01-19 14:21:00 +0700

    changed versioning positioning

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    Version {{ version }}, User Agent {{ userAgent }}
-    <nav @click="goBack" v-if="$route.name !== 'welcome'" class="go-back-container fixed h-16">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-16" fill="white" viewBox="0 0 24 24" stroke="black">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
-        </svg>
-    </nav>
-    <router-view />
-    <!-- add back button here -->
-  </div>
+    <div>
+        <div class="absolute text-white">
+            Version {{ version }}, User Agent {{ userAgent }}
+        </div>
+        <nav @click="goBack" v-if="$route.name !== 'welcome'" class="go-back-container fixed h-16">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-16" fill="white" viewBox="0 0 24 24" stroke="black">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+            </svg>
+        </nav>
+        <router-view />
+        <!-- add back button here -->
+    </div>
 </template>
 
 <script>
